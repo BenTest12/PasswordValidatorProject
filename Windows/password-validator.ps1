86ba53382bf536e5a0e446b640230dab002483f5
V4riableZ <benben95939@gmail.com>
2022-05-25 04:24:16 +0300

    Powershell script to read password input

--- Windows/password-validator.ps1
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env pwsh
-
-$password=$args[0]
-
-if($password.Length -ge 10){
-
-    Write-Host -BackgroundColor Black -ForegroundColor Green "Strong password (b^_^)b"
-}
---
+++ password-validator.ps1
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env pwsh
+
+$password=$args[0]
+
+if($password.Length -ge 10){
+    If ($password -match "[0-9]"){
+        If ($password -cmatch "[A-Z]") {
+            If ($password -cmatch "[a-z]") {
+                Write-Host -BackgroundColor Black -ForegroundColor Green "Strong password (b^_^)b"
+            }else{
+                Write-Host -BackgroundColor Black -ForegroundColor Red "Please include a Lower case letter"}
+        }else{
+            Write-Host -BackgroundColor Black -ForegroundColor Red "Please include an Upper case letter"}
+    }else{
+        Write-Host -BackgroundColor Black -ForegroundColor Red "Please include numbers in your password"}
+}else{
+    Write-Host -BackgroundColor Black -ForegroundColor Red  "Password not accepted , Password lenght should be greater than or equal 10"}
